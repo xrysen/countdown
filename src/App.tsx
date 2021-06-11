f0cf052eeca3c051cded466e5026708ccac28733
Sean Oyler <xrysen@gmail.com>
2021-06-11 15:18:05 -0700

    Adds reactfire and firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import EventList from "./components/EventList";
 
 function App() {
   return (
     <div className="App">
       <h1>Countdown</h1>
+      <EventList />
     </div>
   );
 }
---
+++ src/components/EventList.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+
+const EventList = () => {
+  const [events, setEvents] = useState<any>([]);
+
+ 
+
+
+  return (
+    <div>
+      <h2>Events:</h2>
+      <ol>
+        
+      </ol>
+    </div>
+  )
+}
+
+export default EventList;
